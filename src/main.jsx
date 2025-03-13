208e9df69586b34561a938fe3a582ffc6bd97585
Bắc <you@example.com>
2025-03-14 05:23:10 +0700

    config react router rom

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+ 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ]
+  },
+ ]);
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  
+ <RouterProvider router={router} />
   </StrictMode>,
 )
+
+
+ 
+
