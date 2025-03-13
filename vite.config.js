e2f8324754c2ed77928483592d14ae2fedd7876b
Bắc <you@example.com>
2025-03-14 05:09:37 +0700

    add file doc.txt and file .env , config port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Chạy server tại cổng 3000
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Proxy API đến server backend
+        changeOrigin: true,
+      },
+    },
+  },
+
 })
